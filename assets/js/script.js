e1b4ae12d6f19f92c0d0742ca74e0d589e0caccc
Oscar <oscarwebster96@gmail.com>
2024-08-01 00:14:17 -0400

    se agrega nav presetnacion y habilidades

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
